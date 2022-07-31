45f5b5aaa489dffd57287a754e8d595b3c9e0c53
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-07-30 23:37:02 -0500

    Creación de nueva clase de repositorio que use los mapperstruct

--- src/main/java/co/clarape/al/allsfv2/domain/repository/ProductRepository.java
+++ src/main/java/co/clarape/al/allsfv2/domain/repository/ProductRepository.java
@@ -8,7 +8,7 @@ import co.clarape.al.allsfv2.domain.Product;
 public interface ProductRepository {
     List<Product> getAll();
     Optional<List<Product>> getByCagegory(int categoryId);
-    Optional<List<Product>> getScoreProducts(int quantity);
+    Optional<List<Product>> getScarseProducts(int quantity);
     Optional<Product> getProduct(int productId);
     Product save(Product product);
     void delete(int productId);
